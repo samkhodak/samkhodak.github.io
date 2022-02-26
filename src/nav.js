50eed2f76692b7738bde33b33d14ba8833500330
khod2 <khod2@pdx.edu>
2022-02-25 17:00:03 -0800

    Main page styles done - small changes expected

--- src/nav.js
+++ src/nav.js
@@ -4,6 +4,12 @@ let nav_button = document.getElementById('hamburger');
 let nav_ul = document.getElementById('nav-list');
 
 
+//Hide/display the nav list when the hamburger is pressed.
 nav_button.addEventListener('click', () => {
+    nav_ul.classList.toggle('list');
+});
+
+//Once a link is selected, close the nav list.
+nav_ul.addEventListener('click', () => {
     nav_ul.classList.toggle('list');
 });
