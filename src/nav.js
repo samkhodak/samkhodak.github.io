fdf538a438aa6d2a217984b569601747c8673c8a
samkhodak <samkhodak@gmail.com>
2022-03-05 15:49:17 -0800

    Updated navbar. Incomplete. Added images to repo.

--- src/nav.js
+++ src/nav.js
@@ -1,15 +1,30 @@
 //display list when button is clicked
 
-let nav_button = document.getElementById('hamburger');
-let nav_ul = document.getElementById('nav-list');
+var nav_button = document.getElementById("hamburger");
+var nav_ul = document.getElementById("nav-list");
 
+var window_size = window.matchMedia("(min-width: 1023px)");
 
-//Hide/display the nav list when the hamburger is pressed.
-nav_button.addEventListener('click', () => {
-    nav_ul.classList.toggle('list');
+nav_button.addEventListener("click", () => {
+    nav_ul.classList.toggle("active");
 });
 
-//Once a link is selected, close the nav list.
-nav_ul.addEventListener('click', () => {
-    nav_ul.classList.toggle('list');
-});
+document.querySelectorAll(".nav-link").forEach(i => i.addEventListener("click", () => {
+    nav_ul.classList.remove("active");
+}));
+
+// function nav_toggle () {
+//   nav_ul.classList.toggle("list");
+// }
+
+// //Hide/display the nav list when the hamburger is pressed.
+// nav_button.addEventListener("click", nav_toggle());
+
+// //Once a link is selected, close the nav list.
+// nav_ul.addEventListener("click", nav_toggle());
+
+// window_size.onchange = (e) => {
+//   if (e.matches) {
+
+//   }
+// };
